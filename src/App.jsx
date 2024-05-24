355049401eef0681489075f35b3370e09dfdc525
Taimoor Hussain <taimoorhussain985@gmail.com>
2024-05-24 12:00:03 +0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 const App = () => {
-  return <Signup />;
+  return <Login />;
 };
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,13 @@
 import Input from "./Input";
-const Signup = () => {
+const Login = () => {
   return (
     <div className="flex justify-center mt-10">
       <div className="container h-[500px] w-[350px] md:w-[500px] lg:w-[500px] bg-red-300 text-white rounded-2xl">
-        <div className="font-bold text-4xl mt-4 p-3 text-white">Welcome!</div>
+        <div className="font-bold text-4xl mt-8 p-3 text-white text-center">
+          Login
+        </div>
         <div>
-          <form action="" className="flex flex-col gap-4 items-center mt-4">
-            <Input
-              type="text"
-              placeholder="Name"
-              className="p-3 rounded-lg w-3/4 bg-slate-100"
-            />
-
+          <form action="" className="flex flex-col gap-4 items-center mt-8">
             <Input
               type="email"
               className="p-3 rounded-lg w-3/4  bg-pink-50"
@@ -24,12 +20,12 @@ const Signup = () => {
             />
           </form>
         </div>
-        <div className="flex justify-center mt-5 ">
+        <div className="flex flex-col items-center mt-5 ">
           <button
             className="bg-red-400 text-white m hover:bg-red-500 font-bold px-4 py-3 w-[300px] rounded-lg mt-4"
             type="submit"
           >
-            Register
+            Login
           </button>
           <button
             className="bg-red-400 text-white m hover:bg-red-500 font-bold px-4 py-3 w-[300px] rounded-lg mt-4"
@@ -42,9 +38,17 @@ const Signup = () => {
         <div className="text-center text-white font-bold mt-2">
           Or log in with
         </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="bg-amber-300 text-black hover:bg-amber-400 font-bold px-4 py-3 w-[300px] rounded-lg mt-2"
+          >
+            Log in with Google
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Signup;
+export default Login;
